da96b9235e0aae605ad365825fd102abe0394e9f
fullstack290623 <fullstack290623@gmail.com>
2024-06-09 18:29:34 +0300

    .

--- porj96/src/com/company/Main.java
+++ porj96/src/com/company/Main.java
@@ -5,7 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
         Person danny = new Person("001", "danny", 18, 50.5f);
-        danny.age = -1;
+        Person johnDoe = new Person();
+        johnDoe.setAge(25);
+
+        System.out.println("dannys age is " + danny.getAge());
+        danny.setAge(-1);
+        System.out.println("dannys age after the change is " + danny.getAge());
+
         System.out.println(danny);
 
     }
--- porj96/src/com/company/Person.java
+++ porj96/src/com/company/Person.java
@@ -1,17 +1,79 @@
 package com.company;
 
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+
+//@AllArgsConstructor
+//@ToString
 public class Person {
 
-    String id;
-    String name;
-    int age;
-    float weight;
+//    @Getter
+//    @Setter
+    private String id;
+    private String name;
+    private int age;
+    private float weight;
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int new_age) {
+        if (new_age > 0 && new_age < 120) {
+            this.age = new_age;
+        }
+    }
+
+//    setDistance(float miles) {
+//        this.km = miles * 1.6;
+//    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.length() >= 2)
+            this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        if (weight > 0 && weight < 500)
+            this.weight = weight;
+    }
 
     public Person(String id, String name, int age, float weight) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.weight = weight;
+    }
+
+    public Person(String id, String name, int age) {
+        this(id, name, age, 80f);
+    }
+
+    // empty ctor.
+    public Person() {
+        this.name = "john";
+        this.id = "000";
+        this.age = 1;
+        this.weight = 1f;
     }
 
     @Override
